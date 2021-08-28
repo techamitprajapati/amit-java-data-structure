52cde88ea56b5ac8a218d856622e0c5f18cb035d
Your Name <you@example.com>
2021-08-28 16:43:39 +0530

    amit commit

--- src/datastructure/practices/list/arraylist/ArrayLists.java
+++ src/datastructure/practices/list/arraylist/ArrayLists.java
@@ -3,8 +3,10 @@ package datastructure.practices.list.arraylist;
 import java.util.ArrayList;
 
 public class ArrayLists {
-	ArrayList<String> alist= new ArrayList();
+
 	public static void main(String[] args) {
+		ArrayList<String> alist = new ArrayList();
+		System.out.println(alist);
 	}
 
 }
