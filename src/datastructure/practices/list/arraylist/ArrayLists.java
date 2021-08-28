e77893e9cb4b48fd41b362e6bb9bee48f29f1db4
techprakashmaurya <techprakashmaurya@gmail.com>
2021-08-28 16:14:56 +0530

    Enhancement ArrayList

--- src/datastructure/practices/list/arraylist/ArrayLists.java
+++ src/datastructure/practices/list/arraylist/ArrayLists.java
@@ -1,7 +1,9 @@
 package datastructure.practices.list.arraylist;
 
+import java.util.ArrayList;
+
 public class ArrayLists {
-	ArrayList<String> alist= new ArrayListJ();
+	ArrayList<String> alist= new ArrayList();
 	public static void main(String[] args) {
 	}
 
