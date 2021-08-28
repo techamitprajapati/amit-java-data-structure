a693ad298acd2b57a77309206c0ae73ecd7a376f
Your Name <you@example.com>
2021-08-28 15:37:22 +0530

    code commit in map test

--- src/datastructure/practices/list/arraylist/ArrayLists.java
+++ src/datastructure/practices/list/arraylist/ArrayLists.java
@@ -1,8 +1,14 @@
 package datastructure.practices.list.arraylist;
 
+//import java.util.ArrayList;
+
 public class ArrayLists {
-	ArrayList<String> alist= new ArrayListJ();
+//	ArrayList<String> alist = new ArrayList();
+
 	public static void main(String[] args) {
 	}
 
+	public void mytestmethod() {
+		System.out.println("printing void");
+	}
 }
--- src/datastructure/practices/map/test/MapTest.java
+++ src/datastructure/practices/map/test/MapTest.java
@@ -3,8 +3,7 @@ package datastructure.practices.map.test;
 public class MapTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hashmap table");
 	}
 
 }
